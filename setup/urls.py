a21a0a6eb657c8af5d5eee4802379e930d183b9d
CIROBRUNO90 <cezebruno@gmail.com>
2024-10-27 12:31:20 -0300

    feature: tasks module

--- setup/urls.py
+++ setup/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 from users.urls import urlpatterns as users_urls
+from tasks.urls import urlpatterns as tasks_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += users_urls
+urlpatterns += tasks_urls
